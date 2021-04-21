94ae6eb0fab99482bbfbf5640438ce1b2de470d0
dcabulo <diego.cabulo@linktic.co>
2021-04-21 00:16:03 -0500

    finish the course

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,16 @@
+import BlogList from "./Bloglist";
+import useFetch from "./useFetch";
+
 const Home = () => {
+  const { data: blog, isLoading, error } = useFetch(
+    "http://localhost:8000/blogs"
+  );
+
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      {error && <div>{error}</div>}
+      {isLoading && <div>Loading....</div>}
+      {blog && <BlogList blogs={blog} title={"Diegos blogs"} />}
     </div>
   );
 };
